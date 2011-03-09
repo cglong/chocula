db363b3afed329a6077c7df14c2a3bb17ecd9b02
frankensteinye@gmail.com <frankensteinye@gmail.com>
2011-03-09 17:45:24 +0000

    Getter and Setter of Doctor's office toString not finished

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,4 +1,33 @@
-
+/**
+ * 
+ * @author Ye Zhefan
+ * 
+ */
 public class Doctor extends Nurse implements IDoctor {
+	private String name;
+	private int id;
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Needs to be implemented
+	 */
+	public String toString() {
+		return "";
+	}
 
 }
