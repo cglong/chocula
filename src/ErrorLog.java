35e4d987638fa0538bb824fa68f43ef0670c3ef3
christopherglong@mac.com <christopherglong@mac.com>
2011-03-09 04:54:05 +0000

    Write basic framework of ErrorLog

--- src/ErrorLog.java
+++ src/ErrorLog.java
@@ -1,4 +1,28 @@
+import java.util.ArrayList;
 
 public class ErrorLog {
-
+	private static ArrayList<ErrorEntry> errors;
+	
+	public void unexpectedException(Exception e) {
+		newException(e);
+	}
+	
+	public void expectedException(Exception e) {
+		newException(e);
+		// TODO Display dialog window with message
+	}
+	
+	private void newException(Exception e) {
+		ErrorEntry errorEntry = new ErrorEntry(e);
+		errors.add(errorEntry);
+	}
+	
+	private class ErrorEntry {
+		@SuppressWarnings("unused")
+		private Exception e;
+		
+		public ErrorEntry(Exception e) {
+			this.e = e;
+		}
+	}
 }
