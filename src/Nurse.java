f8aedaca2d5a1c24f6f4995a9ef3b8c715779665
frankensteinye@gmail.com <frankensteinye@gmail.com>
2011-03-09 17:45:56 +0000

    Getter and Setter of Nurse toString not finished

--- src/Nurse.java
+++ src/Nurse.java
@@ -1,4 +1,34 @@
-
+/**
+ * 
+ * @author Ye Zhefan - M5 CRUD appointment and treatment record
+ * 
+ */
 public class Nurse extends User {
+	private String name;
+	private int id;
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * Needs to be implemented
+	 * 
+	 * @return
+	 */
+	public String toStirng() {
+		return "";
+	}
 }
