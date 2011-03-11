c0298efb064b61ac2b5e895e623e36143a2047cb
christopherglong@mac.com <christopherglong@mac.com>
2011-03-11 17:20:36 +0000

    New PatientInformation class that holds Patient's name and other information

---
+++ src/PatientInformation.java
@@ -0,0 +1,12 @@
+
+public class PatientInformation implements Record {
+	private String name;
+	
+	public PatientInformation(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
