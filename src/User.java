c24fd36b335c8bca3ddd944bd1f552423b0ec49d
christopherglong@mac.com <christopherglong@mac.com>
2011-03-11 17:06:07 +0000

    Complete User class

--- src/User.java
+++ src/User.java
@@ -1,4 +1,27 @@
-
+/**
+ * This abstract class represents a general user in the system.
+ * 
+ * @author Christopher Long
+ * @version 1.0
+ */
 public abstract class User {
-
+	private String username, password;
+	
+	/**
+	 * This getter returns the user's username.
+	 * 
+	 * @return The username of the user.
+	 */
+	public String getUsername() {
+		return username;
+	}
+	
+	/**
+	 * This setter returns the user's password.
+	 * 
+	 * @return The password of the user.
+	 */
+	public String getPassword() {
+		return password;
+	}
 }
