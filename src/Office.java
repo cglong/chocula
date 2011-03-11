f26929d9d308fe562d329152d857b866530593a6
christopherglong@mac.com <christopherglong@mac.com>
2011-03-11 21:50:28 +0000

    Find patient by name

--- src/Office.java
+++ src/Office.java
@@ -7,4 +7,20 @@ public class Office {
 	public Office() {
 		users = new ArrayList<User>();
 	}
+	
+	/**
+	 * Finds and returns a patient.
+	 * 
+	 * @param name The name of the patient to find.
+	 * @return The patient with the specified name.
+	 */
+	public Patient findPatient(String name) {
+		for (User user : users)
+			if (user instanceof Patient) {
+				Patient patient = (Patient) user;
+				if (patient.getName().equals(name))
+					return patient;
+			}
+		return null;
+	}
 }
