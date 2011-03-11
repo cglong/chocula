f4557c32c70eb1bab6f935a0bcab58e2cd12f63e
christopherglong@mac.com <christopherglong@mac.com>
2011-03-11 20:14:04 +0000

    Skeleton of Office

--- src/Office.java
+++ src/Office.java
@@ -1,4 +1,10 @@
+import java.util.Collection;
+import java.util.ArrayList;
 
 public class Office {
-
+	private Collection<User> users;
+	
+	public Office() {
+		users = new ArrayList<User>();
+	}
 }
