da69083c4a1f49ff1fba10ba91d2e9026170bb2d
christopherglong@mac.com <christopherglong@mac.com>
2011-03-11 17:25:26 +0000

    Add PatientInformation instance variable and constructor to Patient

--- src/Patient.java
+++ src/Patient.java
@@ -1,4 +1,9 @@
 
 public class Patient extends User implements IPatient {
-
+	private PatientInformation info;
+	
+	public Patient(String username, String password, String name) {
+		super(username, password);
+		this.info = new PatientInformation(name);
+	}
 }
