cd3f84f003503a8920794b906718a1d8caeff74a
frankensteinye@gmail.com <frankensteinye@gmail.com>
2011-03-15 16:14:02 +0000

    Update User, still got an error

--- src/User.java
+++ src/User.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 /**
  * This abstract class represents a general user in the system.
  * 
@@ -6,16 +8,16 @@
  */
 public abstract class User {
 	private String username, password;
-	
+
 	public User(String username, String password) {
 		this.username = username;
 		this.password = password;
 	}
-	
+
 	public User() {
 		this(null, null);
 	}
-	
+
 	/**
 	 * This getter returns the user's username.
 	 * 
@@ -24,7 +26,7 @@ public abstract class User {
 	public String getUsername() {
 		return username;
 	}
-	
+
 	/**
 	 * This setter returns the user's password.
 	 * 
@@ -33,4 +35,10 @@ public abstract class User {
 	public String getPassword() {
 		return password;
 	}
+	
+	public Appointment createAppointment(Date date, Doctor doctor,
+			String reason, Patient patient) {
+		return Office.createAppointment(date, doctor, reason, patient);
+	}
+
 }
