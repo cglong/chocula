e7ed7b7104f4f2562621d7164f4ab779c28389ce
frankensteinye@gmail.com <frankensteinye@gmail.com>
2011-03-15 16:12:50 +0000

    Update Doctor

--- src/Doctor.java
+++ src/Doctor.java
@@ -4,30 +4,5 @@
  * 
  */
 public class Doctor extends Nurse implements IDoctor {
-	private String name;
-	private int id;
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	/**
-	 * Needs to be implemented
-	 */
-	public String toString() {
-		return "";
-	}
 
 }
