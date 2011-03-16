488739488d7fc36f9a3ffe59078c3d0fcbceaf2e
eprado6@gatech.edu <eprado6@gatech.edu>
2011-03-16 02:28:26 +0000

    Made a class that could take care of persistent storage.

---
+++ src/Storage.java
@@ -0,0 +1,20 @@
+/**
+ * @(#)Storage.java
+ *
+ *
+ * @author Elise Prado
+ * @version 1.00 2011/3/15
+ *
+ *
+ * This class takes care of persistent storage... I guess.
+ * Maybe other classes call on it to save their information for them?
+ */
+
+
+public class Storage {
+
+    public Storage() {
+    }
+    
+    
+}
