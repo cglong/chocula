09aca13e9c67688ccd312099029a3209e7653c7a
christopherglong@mac.com <christopherglong@mac.com>
2011-04-01 16:21:02 +0000

    Javadoc for Nurse

--- src/com/googlecode/chocula/core/Nurse.java
+++ src/com/googlecode/chocula/core/Nurse.java
@@ -1,10 +1,18 @@
 package com.googlecode.chocula.core;
 /**
+ * The class represents a nurse
  * 
- * @author Ye Zhefan - M5 CRUD appointment and treatment record
- * 
+ * @author Ye Zhefan
+ * @version 1.0
  */
 public class Nurse extends User {
+	
+	/**
+	 * This constructor creates a nurse
+	 * 
+	 * @param username The username of the nurse
+	 * @param password The password of the nurse
+	 */
 	public Nurse(String username, String password) {
 		super(username, password);
 	}
