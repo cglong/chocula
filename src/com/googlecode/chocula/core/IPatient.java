6f203b73a6a6b458ce1d016e55a45c74bb5558cd
christopherglong@mac.com <christopherglong@mac.com>
2011-04-01 16:20:02 +0000

    Javadoc for IPatient

--- src/com/googlecode/chocula/core/IPatient.java
+++ src/com/googlecode/chocula/core/IPatient.java
@@ -1,5 +1,11 @@
 package com.googlecode.chocula.core;
 
+/**
+ * Interface for users that can perform Patient actions
+ * 
+ * @author Christopher Long
+ * @version 1.0
+ */
 public interface IPatient {
 
 }
