c3b3e4a75a42aadec044cea5a7f075540090bbb8
christopherglong@mac.com <christopherglong@mac.com>
2011-04-07 21:36:05 +0000

    Reverse height and width in MainFrame

--- src/com/googlecode/chocula/client/MainFrame.java
+++ src/com/googlecode/chocula/client/MainFrame.java
@@ -34,7 +34,7 @@ public class MainFrame extends JFrame implements UIInfo {
 	 */
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, WINDOWHEIGHT, WINDOWWIDTH);
+		setBounds(100, 100, WINDOWWIDTH, WINDOWHEIGHT);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
